7e8ddee37037c610e820ecfbec62f90620ee513b
ashanaka <jpashanshanaka@gmail.com>
2019-12-26 08:37:29 +0530

    Database connection is successfull

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 require('./models/db');
+const express = require('express');
 
+
+const app = express();
 
 //Starting the server
 app.listen(5000, () => {
-    console.log('Express server started using port : 3000');
+    console.log('Express server started using port : 5000');
 });
